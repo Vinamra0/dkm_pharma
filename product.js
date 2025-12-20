b550dc0d59cda2ae0a3567354d86096fe1cead15
amanjha21 <aman.jha21.aj@gmail.com>
2025-12-20 22:34:42 +0545

    v6

---
+++ product.js
@@ -0,0 +1,15 @@
+const x = [
+    {
+        id: "123",
+        name: "Test 1",
+        packing: "10 x 15 Tabs",
+        image: "https://via.placeholder.com/150",
+        composition: "Test Comp 1",
+        company: "Test Company 1",
+        category: "Test Cat 1",
+        tags: ["Test Tag 1", "Test Tag 2"],
+        generics: ["Test Name 1", "Test Name 2"],
+        subCategory: "Test Sub Cat 1",
+        packageType: "Vile"
+    }
+]
